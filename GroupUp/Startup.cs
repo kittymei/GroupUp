24be8a7de3610fe9116a6176cc9a87f3eb7654fb
Kitty Mei <MEIK@my.erau.edu>
2019-10-25 09:38:33 -0700

    Website is up on Azure

--- GroupUp/Startup.cs
+++ GroupUp/Startup.cs
@@ -42,6 +42,7 @@ namespace GroupUp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                
             }
             else
             {
